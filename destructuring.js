22561addc86531f5ad32e95826589d07f81589ce
MathFreitas <matheus.freitas_santos@outlook.com>
2021-12-14 16:18:50 -0300

    utilizando destructuring

---
+++ destructuring.js
@@ -0,0 +1,11 @@
+//modo verboso
+var arr = ['Uva', 'Banana', 'Laranja'];
+
+var uva = arr[0];
+var banana = arr[1];
+var laranja = arr[2];
+
+//destructuring assigment
+var [uva, banana, laranja] = ['Uva', 'Banana', 'Laranja'];
+
+console.log(uva, banana, laranja);
