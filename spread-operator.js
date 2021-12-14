0e100d817f4ede44514d32bc260dd6cfb51c92a4
MathFreitas <matheus.freitas_santos@outlook.com>
2021-12-14 15:24:11 -0300

    utilizando spread em string

--- spread-operator.js
+++ spread-operator.js
@@ -1,7 +1,19 @@
-//spread operator c/ lista
+//spread operator
 
 const multi = (...args) => args.reduce((acc, value) => acc * value, 1);
 
 const soma = (...rest) => {
     return multi(...rest);
 };
+
+//spread pode ser utilizado em strings, arrays, literal objects e objetos iteraveis
+
+//spread em string
+const string = 'Digital One';
+
+function logArgs(...args) {
+    console.log(args);
+};
+
+logArgs(...string);
+
