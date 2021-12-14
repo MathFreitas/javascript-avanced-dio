8c09655712b0bf7c884209f9409d737c7400146d
MathFreitas <matheus.freitas_santos@outlook.com>
2021-12-14 16:21:50 -0300

    array dentro de array

--- destructuring.js
+++ destructuring.js
@@ -1,11 +1,12 @@
 //modo verboso
-var arr = ['Uva', 'Banana', 'Laranja'];
+var arr = ['Uva', 'Banana', 'Laranja', ['Tomate']];
 
 var uva = arr[0];
 var banana = arr[1];
 var laranja = arr[2];
+var tomate = arr[3][0];
 
 //destructuring assigment
-var [uva, banana, laranja] = ['Uva', 'Banana', 'Laranja'];
+var [uva, banana, laranja, [tomate]] = ['Uva', 'Banana', 'Laranja', ['Tomate']];
 
-console.log(uva, banana, laranja);
+console.log(uva, banana, laranja, [tomate]);
